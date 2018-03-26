da9b685ebea60b83a9e13d9406dd39b3a6181a2a
Dylan.Qiu <32313210+DylanQiu@users.noreply.github.com>
2018-03-26 11:27:24 -0400

    206

---
+++ 206-reverseLinkedList.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseList(self, head):
+        """
+        reverse a singly linked list
+        :param head: ListNode
+        :return: ListNode
+        """
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return curr
+
