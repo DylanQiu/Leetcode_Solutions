758a8e82ee04128a60bce937c4d73d06182f8ba7
Dylan.Qiu <32313210+DylanQiu@users.noreply.github.com>
2018-07-08 23:59:15 -0400

    5

---
+++ 371-sumOfIntegers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getSum(self, a, b):
+        MAX = 0x7FFFFFFF
+        MIN = 0x80000000
+        mask = 0xFFFFFFFF
+        while b!=0:
+            a, b = (a^b) & mask, ((a&b)<<1) & mask
+
+        return a if a<= MAX else ~(a^mask)
