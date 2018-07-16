e5ec4c4469dddbabfb74ed8f998d1345e13b6606
Dylan.Qiu <32313210+DylanQiu@users.noreply.github.com>
2018-07-15 23:36:04 -0400

    5

---
+++ 414-thirdMaximumNumber.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(set(nums)) < 3:
+            return max(nums)
+        return sorted(list(set(nums)))[-3]
