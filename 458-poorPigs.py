362e998a3b80ec3f8583108951cc08c579baf3f4
Dylan.Qiu <32313210+DylanQiu@users.noreply.github.com>
2018-07-31 23:47:19 -0400

    5

---
+++ 458-poorPigs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        pigs = 0
+        while (minutesToTest/minutesToDie+1)**pigs < buckets:
+            pigs += 1
+        return pigs
+
+a = Solution()
+print a.poorPigs(1000, 15, 60)
