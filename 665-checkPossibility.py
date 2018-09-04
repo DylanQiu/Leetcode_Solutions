0f98dff0077cc9d54b9a95ee2e0f98d37ff0482b
Dylan.Qiu <32313210+DylanQiu@users.noreply.github.com>
2018-09-03 23:43:50 -0400

    575

---
+++ 665-checkPossibility.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        one, two = nums[:], nums[:]
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                one[i] = nums[i+1]
+                two[i+1] = nums[i]
+                break
+        return one == sorted(one) or two == sorted(two)
+
+"""
+class Solution(object):
+    def checkPossibility(self, nums):
+        count=0
+        for i in range(len(nums)-1):
+            if nums[i]>nums[i+1]:
+                count+=1
+                if count>1 or ((i-1>=0 and nums[i-1]>nums[i+1]) and (i+2<len(nums) and nums[i+2]<nums[i])):
+                    return False
+        return True
+"""
